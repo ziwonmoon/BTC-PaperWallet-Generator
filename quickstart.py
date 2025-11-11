7b39754068ca3db9b6e175eaf0928b48ae071bf8
문지원 <ziwonmoon@gmail.com>
2025-11-11 18:30:25 +0900

    main.py 패스워드 전역상수 사용 안하던 거 수정

--- quickstart.py
+++ quickstart.py
@@ -5,7 +5,7 @@ PASSWORD = 'YOUR_PASSWORD'
 
 from BTC_PaperWallet_Generator.KeyManager import KeyManager
 
-mykey = KeyManager(True, None, passwd='PASSWORD')
+mykey = KeyManager(True, None, passwd=PASSWORD)
 
 mykey.get_encrypted_private_key_QR()
 mykey.get_public_address_QR()
